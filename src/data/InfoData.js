3ca575ab05b5951b334061496422f32919a55d83
jackieung451 <jackieung451@gmail.com>
2021-07-04 16:17:01 -0700

    Info Section

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from "../images/img7.jpg";
+import ImageTwo from "../images/img8.jpg";
+
+export const InfoData = {
+  heading: "Explore our beautiful homes",
+  paragraphOne: "lorem",
+  paragraphTwo: "loremmmdsfadfds",
+  buttonLabel: "View Homes",
+  image: ImageOne,
+  reverse: false,
+  delay: 100,
+};
+
+export const InfoDataTwo = {
+  heading: "MOdern Designs",
+  paragraphOne: "lorem",
+  paragraphTwo: "loremmmdsfadfds",
+  buttonLabel: "View Homes",
+  image: ImageTwo,
+  reverse: true,
+  delay: 300,
+};
