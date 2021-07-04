ce02d969bedb9ee2dac74b944db0f356d84bed70
jackieung451 <jackieung451@gmail.com>
2021-07-03 18:50:36 -0700

    First Commit

---
+++ src/data/SliderData.js
@@ -0,0 +1,39 @@
+import ImageOne from "../images/img1.jpg";
+import ImageTwo from "../images/img2.jpg";
+import ImageThree from "../images/img3.jpg";
+import ImageFour from "../images/img4.jpg";
+
+export const SliderData = [
+  {
+    title: "Luxury Villa in Bali, Indonesia",
+    price: "$4280000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageOne,
+    alt: "House",
+  },
+  {
+    title: "House in San Jose, Costa Rica",
+    price: "$2860000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageTwo,
+    alt: "House",
+  },
+  {
+    title: "House in Calabasas, California",
+    price: "$8420000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageThree,
+    alt: "House",
+  },
+  {
+    title: "House in Houston, Texas",
+    price: "$6660000",
+    path: "/homes",
+    label: "View Home",
+    image: ImageFour,
+    alt: "House",
+  },
+];
