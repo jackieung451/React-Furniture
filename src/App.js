10bee0a6ef386bd9c729c2906d79e8567e508dc8
jackieung451 <jackieung451@gmail.com>
2021-07-05 14:13:33 -0700

    Listings and Features Sections

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
 import InfoSection from "./components/InfoSection";
 import Navbar from "./components/Navbar";
-import { InfoData } from "./data/InfoData";
+import { InfoData, InfoDataTwo } from "./data/InfoData";
 import { SliderData } from "./data/SliderData";
 import GlobalStyle from "./globalStyles";
+import Listings from "./components/Listings";
+import Features from "./components/Features";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,6 +23,9 @@ function App() {
       <Dropdown isOpen={isOpen} toggle={toggle} />
       <Hero slides={SliderData} />
       <InfoSection {...InfoData} />
+      <Listings />
+      <Features />
+      <InfoSection {...InfoDataTwo} />
     </>
   );
 }
