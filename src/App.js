c7428256dfc55adc39fd009e82eaf520e739bbbe
jackieung451 <jackieung451@gmail.com>
2021-07-05 19:30:36 -0700

    Animation Effects

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useLayoutEffect } from "react";
 import Dropdown from "./components/Dropdown";
 import Navbar from "./components/Navbar";
 import GlobalStyle from "./globalStyles";
 import Footer from "./components/Footer";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 import Home from "./pages";
 import About from "./pages/About";
 import Rentals from "./pages/Rentals";
 import Homes from "./pages/Homes";
 import Contact from "./pages/Contact";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+
+  console.log(location.pathname);
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    Aos.init({});
+  }, []);
 
   return (
     <>
--- src/components/Features.js
+++ src/components/Features.js
@@ -77,7 +77,13 @@ const Features = () => {
       <Container>
         <Wrap>
           <ColumnLeft>
-            <Content>
+            <Content
+              data-aos="fade-right"
+              data-aos-duration="1200"
+              data-aos-delay="300"
+              data-aos-once="true"
+              data-aos-anchor-placement="center bottom"
+            >
               <h1>Stunning Interior</h1>
               <p>Loerefasfasfasfdsafas</p>
               <p>Lorwerewrfwewewfwa</p>
@@ -85,7 +91,13 @@ const Features = () => {
             </Content>
           </ColumnLeft>
           <ColumnRight>
-            <Image src={ImageOne} />
+            <Image
+              src={ImageOne}
+              data-aos="fade-left"
+              data-aos-duration="1200"
+              data-aos-once="true"
+              data-aos-anchor-placement="center bottom"
+            />
           </ColumnRight>
         </Wrap>
       </Container>
