92bfd78efc87c62e1505bfe8a6609e9bb8c51e74
jackieung451 <jackieung451@gmail.com>
2021-07-21 23:08:40 -0700

    Updated Contac Us page

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Button } from "./Button";
-import ImageOne from "../images/img4.jpg";
+import ImageOne from "../images/img6.jpg";
 
 // const Section = styled.section`
 //   background: #000d1a;
@@ -418,65 +418,65 @@ import ImageOne from "../images/img4.jpg";
 //   );
 // };
 
-const Section = styled.section`
-  width: 100%;
-  height: 100%;
-  padding: 3.5rem 0rem;
-`;
+// const Section = styled.section`
+//   width: 100%;
+//   height: 100%;
+//   padding: 3.5rem 0rem;
+// `;
 
-const Container = styled.div`
-  padding: 3 rem calc((100vw - 1300px) / 2);
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: 800px;
+// const Container = styled.div`
+//   padding: 3 rem calc((100vw - 1300px) / 2);
+//   display: grid;
+//   grid-template-columns: 1fr 1fr;
+//   grid-template-rows: 800px;
 
-  @media screen and (max-width: 768px) {
-    grid-template-columns: 1fr;
-  }
-`;
+//   @media screen and (max-width: 768px) {
+//     grid-template-columns: 1fr;
+//   }
+// `;
 
-const ColumnLeft = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  line-height: 1.4;
-  
-  padding: 0rem 4rem;
-  order: ${({ reverse }) => (reverse ? "2" : "1")};
+// const ColumnLeft = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   line-height: 1.4;
 
-  @media screen and (max-width: 768px) {
-    margin: 0 0 0 0;
-    padding: 0rem 0rem 0rem 0rem;
-  }
+//   padding: 0rem 4rem;
+//   order: ${({ reverse }) => (reverse ? "2" : "1")};
 
-  h1 {
-    margin-bottom: 1rem;
-    font-size: clamp(1.5rem, 6vw, 2rem);
-  }
-  p {
-    ${"" /* margin-bottom: 2rem; */}
-  }
-  
-  }
-`;
-const ColumnRight = styled.div`
-  ${"" /* margin-top: 6rem; */}
-  padding: 1rem 0rem 0rem 0rem;
-  order: ${({ reverse }) => (reverse ? "1" : "2")};
-  display: flex;
-  justify-content: center;
-  align-items: center;
+//   @media screen and (max-width: 768px) {
+//     margin: 0 0 0 0;
+//     padding: 0rem 0rem 0rem 0rem;
+//   }
 
-  @media screen and (max-width: 768px) {
-    order: ${({ reverse }) => (reverse ? "2" : "1")};
-  }
-  iframe {
-    @media screen and (max-width: 768px) {
-      width: 30rem;
-    }
-    ${
-      "" /* img {
+//   h1 {
+//     margin-bottom: 1rem;
+//     font-size: clamp(1.5rem, 6vw, 2rem);
+//   }
+//   p {
+//     ${"" /* margin-bottom: 2rem; */}
+//   }
+
+//   }
+// `;
+// const ColumnRight = styled.div`
+//   ${"" /* margin-top: 6rem; */}
+//   padding: 1rem 0rem 0rem 0rem;
+//   order: ${({ reverse }) => (reverse ? "1" : "2")};
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+
+//   @media screen and (max-width: 768px) {
+//     order: ${({ reverse }) => (reverse ? "2" : "1")};
+//   }
+//   iframe {
+//     @media screen and (max-width: 768px) {
+//       width: 30rem;
+//     }
+
+/* img {
     width: 100%;
     height: 100%;
     object-fit: cover;
@@ -486,7 +486,93 @@ const ColumnRight = styled.div`
       height: 90%;
       margin-top: 75px;
     } */
+
+const Section = styled.section`
+  background: #000d1a;
+  padding: 3.5rem 0rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  background: #fff;
+  padding: 3rem 2rem;
+  position: relative;
+`;
+
+const Wrap = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+`;
+const ColumnLeft = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0px -15px;
+  justify-content: flex-start;
+  padding: 1rem;
+
+  @media screen and (max-width: 768px) {
+    iframe {
+      min-width: 200px;
+      max-width: 1020px;
     }
+  }
+`;
+
+const Content = styled.div`
+  flex: 0 0 50%;
+  line-height: 1.8;
+
+  @media screen and (max-width: 768px) {
+    flex: 0 0 100%;
+    max-width: 100%;
+    margin-top: 250px;
+  }
+
+  h1 {
+    ${"" /* margin-bottom: 2rem; */}
+    font-size: 2 rem;
+    display: flex;
+    justify-content: flex-start;
+  }
+
+  p {
+    margin-bottom: 1rem;
+    line-height: 1.5;
+  }
+  h2 {
+    display: flex;
+    justify-content: center;
+  }
+`;
+const ColumnRight = styled.div`
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  max-width: 850px;
+  height: 100%;
+  width: 45%;
+  padding-left: 1rem;
+
+  @media screen and (max-width: 768px) {
+    height: 320px;
+    top: -65px;
+    width: 80%;
+    margin: 0 auto;
+    left: 0;
+  }
+`;
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  @media screen and (max-width: 768px) {
+    ${"" /* flex: 0 0 100%; */}
+    max-width: 100%;
+
+    margin-top: 60px;
+    padding-bottom: 40px;
   }
 `;
 
@@ -501,25 +587,33 @@ const Features = ({
   return (
     <Section>
       <Container>
-        <ColumnLeft>
-          <h1>Nuestra Tienda</h1>
-          <p>2619 N Broadway</p>
-          <p>Los Angeles, California 90031</p>
-          <p>
-            <strong>(323) 247-9094</strong>
-          </p>
-          <p>sales@lanayaritfurniture.com</p>
-        </ColumnLeft>
-        <ColumnRight reverse={reverse}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13219.317769356978!2d-118.2149471!3d34.0738862!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd15fe4935319e285!2sLa%20Nayarit%20Furniture!5e0!3m2!1sen!2sus!4v1626564967160!5m2!1sen!2sus"
-            width="600"
-            height="450"
-            style={{ border: 0 }}
-            allowfullscreen=""
-            loading="lazy"
-          ></iframe>
-        </ColumnRight>
+        <Wrap>
+          <ColumnLeft>
+            <Content>
+              <h1>Nuestra Tienda</h1>
+              <p>2619 N Broadway</p>
+              <p>Los Angeles, California 90031</p>
+              <p>
+                <strong>(323) 247-9094</strong>
+              </p>
+              <p>sales@lanayaritfurniture.com</p>
+            </Content>
+            <br />
+            <br />
+            <br />
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13219.317769356978!2d-118.2149471!3d34.0738862!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xd15fe4935319e285!2sLa%20Nayarit%20Furniture!5e0!3m2!1sen!2sus!4v1626564967160!5m2!1sen!2sus"
+              width="600"
+              height="450"
+              style={{ border: 0 }}
+              allowfullscreen=""
+              loading="lazy"
+            ></iframe>
+          </ColumnLeft>
+          <ColumnRight>
+            <Image src={ImageOne} alt="recruiting" />
+          </ColumnRight>
+        </Wrap>
       </Container>
     </Section>
   );
